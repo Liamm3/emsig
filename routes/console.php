7c37719f666577266ff823c4af0c34d211fd669c
Liamm3 <liam.wohlfart@tuta.io>
2024-10-20 16:25:18 +0200

    feat: add peak starter

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Schedule::command('cache:clear')->daily();
+// Schedule::command('config:cache')->daily();
+// Schedule::command('route:cache')->daily();
+// Schedule::command('statamic:stache:warm')->daily();
+// Schedule::command('statamic:static:clear')->daily();
+// Schedule::command('statamic:static:warm')->daily();
